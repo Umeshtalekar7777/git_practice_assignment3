87171df5a621684b6bc2b9069b83c4d415a8dea3
umeshtalekar7777 <umeshtalekar7777@gmail.com>
2022-09-20 12:57:39 +0530

    second change

--- prime_problem.js
+++ prime_problem.js
@@ -2,14 +2,14 @@
 
 let num = 13;
 
-let factors = 0;
+let factorials = 0;
 
 for (let i = 2; i < num; i++) {
   if (num % i == 0) {
-    factors++;
+    factorials++;
   }
 }
-if (factors > 0) {
+if (factorials > 0) {
   console.log(num, "Is a prime");
 } else {
   console.log(num, "Is not prime");

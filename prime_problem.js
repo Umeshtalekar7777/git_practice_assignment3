d6a5bfa3ebc9a1a2b7472bb6ff80f7499020ad4f
cnikita26 <nikitachavan3337@gmail.com>
2022-09-20 12:51:59 +0530

    Change variable update m2_code_fix

--- prime_problem.js
+++ prime_problem.js
@@ -2,13 +2,13 @@
 
 let num = 13;
 
-let flag = false;
+let ans = false;
 for (let i = 2; i < num; i++) {
   if (num % i == 0) {
-    flag = true;
+    ans = true;
   }
 }
-if (flag==true) {
+if (ans==true) {
   console.log(num, "Is a prime");
 } else {
   console.log(num, "Is not prime");
